2f81c1ff3554cfb61837d834065a9b1a5891dc62
agakhil13 <aguptaji1310@gmail.com>
2022-04-14 10:24:56 +0530

    migrate to db - created table

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -22,5 +22,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('travello.urls')),
-    path('calc',include('calc.urls')),
+    path('calc/',include('calc.urls')),
 ]
